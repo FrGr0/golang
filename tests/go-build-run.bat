0f1969b732a6e2394d3ce55527c04a2405a35aa5
FrGr <40703243+frgr-bakemono@users.noreply.github.com>
2019-03-13 11:00:55 +0100

    Add files via upload

---
+++ tests/go-build-run.bat
@@ -0,0 +1,14 @@
+@echo off
+
+set PATH=%PATH%;C:\apps\go\bin;C:\apps\MinGW\mingw32\bin
+set GOPATH=%GOPATH%;C:\FG\DEV-GO\lib
+
+echo compilation: %1
+
+go build "%1"
+
+echo execution: %~n1.exe
+
+call "%~n1.exe"
+
+pause
